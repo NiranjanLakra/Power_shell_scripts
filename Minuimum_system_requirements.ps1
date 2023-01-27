76468d35c03e30567cfa65c6a5ad10471dfa2673
NiranjanLakra <niranjan.lakra19@gmail.com>
2023-01-28 02:50:32 +0530

    First Commit

---
+++ Minuimum_system_requirements.ps1
@@ -0,0 +1,33 @@
+﻿$supported_os_list = 'Windows 7 Starter','Windows 7 Premium','Windows 7 Home Basic','Windows 7 Ultimate','Windows 7 Professional','Windows 7 Enterprise','Windows 10 Home','Windows 10 Pro','Windows 10 Pro for Workstation','Windows 10 Enterprise','Windows 10 Education','Microsoft Windows 11 Pro','Microsoft Windows 11 Home','Microsoft Windows 11 Pro for Education','Microsoft Windows 11 Workstation','Microsoft Windows 11 Enterprise','Microsoft Windows 11 Education'
+$supported_platform = 'amd64'
+$supported_build_workstations = '22621'
+Write-Host -ForegroundColor Cyan 'CHECKING SYSTEM COMPATIBILITY'
+	Write-Host ' '
+	$os_name = (Get-WmiObject win32_operatingsystem).Caption
+	$ARCH = ($env:PROCESSOR_ARCHITECTURE).ToLower()
+    $build_ID = [System.Environment]::OSVersion.Version.Build
+	if($supported_os_list -contains $os_name){
+		[char]8730 + ' ' + $os_name +' OS supported '
+        if($supported_platform -contains ($ARCH)){
+		    [char]8730 + ' Architecture ' + $ARCH + ' supported '
+            if( $supported_build_workstations -contains $build_ID){
+                [char]8730 + ' ' + $build_ID + ' Build is supported '
+                if($NETVersion = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where { $_.PSChildName -eq 'Full'} | Select version){
+                    [char]8730 + ' ' +".NET $NETVersion "
+                    Write-Host 'Checking updates from microsoft Servers Hold ON!!!'
+                    $updatesession =  [activator]::CreateInstance([type]::GetTypeFromProgID('Microsoft.Update.Session',$env:computername))
+	                $updatesearcher = $updatesession.CreateUpdateSearcher()
+	                $searchresult = $updatesearcher.Search("IsInstalled=0")
+	                if($searchresult.Updates.Count -gt 0){
+		                ($searchresult.Updates.Count).ToString() + ' Updates Available ' + [char]8855
+                        control update
+	                }
+	                else{[char]8730 + ' ' +'System is fully Updated '}  
+                }
+                else{Write-Host '.NET seems not installed'}
+            }
+            else{'OS Build not supported '}
+	    }
+	    else{}
+	}
+	else{'OS not supported'}
---
+++ Windows_platforms_json_Object.ps1
@@ -0,0 +1,111 @@
+$supported_platforms = @{
+
+    'Windows 7' = @{
+        'Windows 7 Starter' = @{
+            'Arch'= @('x86')
+            'SP' = @('SP1','pre-SP1')
+        }
+        'Windows 7 Home Basic' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('SP1','pre-SP1')
+        }
+        'Windows 7 Premium' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('SP1','pre-SP1')
+        }
+        'Windows 7 Ultimate' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('SP1','pre-SP1')
+        }
+        'Windows 7 Professional' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('SP1','pre-SP1')
+        }
+        'Windows 7 Enterprise' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('SP1','pre-SP1')
+        }
+
+    }
+    'windows 8' = @{
+        'Windows 8 Home' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('pre-SP1')
+        }
+        'Windows 8 Pro' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('pre-SP1')
+        }
+        'Windows 8 Enterprise' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('pre-SP1')
+        }
+    }
+    'windows 8.1' = @{
+        'Windows 8.1 Home' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('pre-SP1')
+        }
+        'Windows 8.1 Pro' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('pre-SP1')
+        }
+        'Windows 8.1 Enterprise' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('pre-SP1')
+        }
+    }
+    'windows 10' = @{
+        'windows 10 Home' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('Semi-Annual 19043', 'General Availability 19044','General Availability 19045')
+        }
+        'windows 10 Pro' = @{
+            'Arch' = @('x86-64','ARM')
+            'SP' = @('Semi-Annual 19043', 'General Availability 19044','General Availability 19045')
+        }
+        'windows 10 Pro for Workstation' = @{
+            'Arch' = @('x86-64','ARM')
+            'SP' = @('Semi-Annual 19043', 'General Availability 19044','General Availability 19045')
+        }
+        'windows 10 Enterprise' = @{
+            'Arch' = @('x86-64','ARM')
+            'SP' = @('Semi-Annual 19042','Semi-Annual 19043', 'General Availability 19044','General Availability 19045', 'LTSB 1507 RTM','LTSB 1507 1607','LTSC 1809','LTSC 19044')
+        }
+        'windows 10 Education' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('Semi-Annual 19042','Semi-Annual 19043', 'General Availability 19044','General Availability 19045')
+        }
+    }
+    'windows 11' = @{
+        'windows 11 Home' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('22000','22621')
+        }
+        'windows 11 Pro' = @{
+            'Arch' = @('x86-64','ARM')
+            'SP' = @('22000','22621')
+        }
+        'windows 11 Pro for Education' = @{
+            'Arch' = @('x86-64','ARM')
+            'SP' = @('22000','22621')
+        }
+        'windows 11 Workstation' = @{
+            'Arch' = @('x86-64','ARM')
+            'SP' = @('22000','22621')
+        }
+        'windows 11 Enterprise' = @{
+            'Arch' = @('x86-64','ARM')
+            'SP' = @('22000','22621')
+        }
+        'windows 11 Education' = @{
+            'Arch' = @('x86-64')
+            'SP' = @('22000','22621')
+        }
+
+    }
+}
+
+$supported = $supported_platforms | ConvertTo-Json
+$errr = $null
+$MyJsonObject = [Microsoft.PowerShell.Commands.JsonObject]::ConvertFromJson($supported, [ref]$errr)
